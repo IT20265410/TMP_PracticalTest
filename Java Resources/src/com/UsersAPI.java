5aa5ac6a32b48b508e66fc427ed30ec18a6f31ce
thuli99 <thulyapremathilaka@gmail.com>
2022-05-11 02:18:35 +0530

    Servlet UsersAPI.java

--- Java Resources/src/com/UsersAPI.java
+++ Java Resources/src/com/UsersAPI.java
@@ -1,6 +1,12 @@
 package com;
 
+import com.User;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,43 +19,75 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UsersAPI")
 public class UsersAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UsersAPI() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	User userObj = new User();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public UsersAPI() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		// not used
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String output = userObj.register(request.getParameter("firstName"), request.getParameter("lastName"),
+				request.getParameter("NIC"), request.getParameter("address"), request.getParameter("phone"),
+				request.getParameter("email"));
+		response.getWriter().write(output);
+	}
+
+	private static Map getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		} catch (Exception e) {
+		}
+		return map;
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map paras = getParasMap(request);
+		String output = userObj.updateUser(paras.get("hidUserIDSave").toString(), paras.get("firstName").toString(),
+				paras.get("lastName").toString(), paras.get("NIC").toString(), paras.get("address").toString(),
+				paras.get("phone").toString(), paras.get("email").toString());
+		response.getWriter().write(output);
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map paras = getParasMap(request); 
+		 String output = userObj.deleteUser(paras.get("userId").toString()); 
+		response.getWriter().write(output); 
 	}
 
 }
