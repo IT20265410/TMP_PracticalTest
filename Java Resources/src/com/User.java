d91eed4822dccaf17426973fdbcab2a55c633e8d
thuli99 <thulyapremathilaka@gmail.com>
2022-05-11 02:45:40 +0530

    modify update method

--- Java Resources/src/com/User.java
+++ Java Resources/src/com/User.java
@@ -17,72 +17,73 @@ public class User {
 		}
 		return con;
 	}
-	
-	//admin side view all customers
-		public String readUsers() {
-			String output = "";
-			try {
-				Connection con = connect();
-				if (con == null) {
-					return "Error while connecting to the database for reading.";
-				}
-				
-				// Prepare the html table to be displayed
-				output = "<table border='1'><tr><th>First Name</th> <th>Last Name</th><th>NIC</th>"
-						+ "<th>Address</th><th>Phone</th><th>Emai</th><th>Update</th><th>Remove</th></tr>";
 
-				String query = "select * from user";
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-				
-				// iterate through the rows in the result set
-				while (rs.next()) {
-					String userId = Integer.toString(rs.getInt("userId"));
-					String firstName = rs.getString("firstName");
-					String lastName = rs.getString("lastName");
-					String NIC = rs.getString("NIC");
-					String address = rs.getString("address");
-					String phone = rs.getString("phone");
-					String email = rs.getString("email");
-					
-					// Add into the html table
-					output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + userId
-							+ "'>" + firstName + "</td>";
-					output += "<td>" + lastName + "</td>";
-					output += "<td>" + NIC + "</td>";
-					output += "<td>" + address + "</td>";
-					output += "<td>" + phone + "</td>";
-					output += "<td>" + email + "</td>";
-					
-					// buttons
-					output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
-							+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-userid='"
-							+ userId + "'>" + "</td></tr>";
-				}
-				con.close();
-				// Complete the html table
-				output += "</table>";
-			} catch (Exception e) {
-				output = "Error while reading the customers details.";
-				System.err.println(e.getMessage());
+	// admin side view all customers
+	public String readUsers() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
 			}
-			return output;
-		}
 
-	//user add method
-	public String register(String fname, String lname, String nic, String address , String phone, String email) {
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>First Name</th> <th>Last Name</th><th>NIC</th>"
+					+ "<th>Address</th><th>Phone</th><th>Emai</th><th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from user";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String userId = Integer.toString(rs.getInt("userId"));
+				String firstName = rs.getString("firstName");
+				String lastName = rs.getString("lastName");
+				String NIC = rs.getString("NIC");
+				String address = rs.getString("address");
+				String phone = rs.getString("phone");
+				String email = rs.getString("email");
+
+				// Add into the html table
+				output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + userId
+						+ "'>" + firstName + "</td>";
+				output += "<td>" + lastName + "</td>";
+				output += "<td>" + NIC + "</td>";
+				output += "<td>" + address + "</td>";
+				output += "<td>" + phone + "</td>";
+				output += "<td>" + email + "</td>";
+
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' "
+						+ "class='btnUpdate btn btn-secondary' data-userid='" + userId + "'></td>"
+						+ "<td><input name='btnRemove' type='button' value='Remove' "
+						+ "class='btnRemove btn btn-danger' data-userid='" + userId + "'></td></tr>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the customers details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	// user add method
+	public String register(String fname, String lname, String nic, String address, String phone, String email) {
 		String output = "";
 		try {
 			Connection con = connect();
 			if (con == null) {
 				return "Error while connecting to the database for inserting.";
 			}
-			
+
 			// create a prepared statement
 			String query = " insert into user (`userId`,`firstName`,`lastName`,`NIC`,`address`, `phone`, `email`)"
 					+ " values (?, ?, ?, ?, ?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
+
 			// binding values
 			preparedStmt.setInt(1, 0);
 			preparedStmt.setString(2, fname);
@@ -91,7 +92,7 @@ public class User {
 			preparedStmt.setString(5, address);
 			preparedStmt.setString(6, phone);
 			preparedStmt.setString(7, email);
-			
+
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
@@ -103,10 +104,10 @@ public class User {
 		}
 		return output;
 	}
-	
-	
-	//update customer details
-	public String updateUser(String userid, String fname, String lname, String nic, String address, String phone, String email) {
+
+	// update customer details
+	public String updateUser(String userid, String fname, String lname, String nic, String address, String phone,
+			String email) {
 		String output = "";
 		try {
 			Connection con = connect();
@@ -124,7 +125,7 @@ public class User {
 			preparedStmt.setString(5, phone);
 			preparedStmt.setString(6, email);
 			preparedStmt.setInt(7, Integer.parseInt(userid));
-			
+
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
@@ -136,9 +137,8 @@ public class User {
 		}
 		return output;
 	}
-	
-	
-	//admin side delete a customer
+
+	// admin side delete a customer
 	public String deleteUser(String userId) {
 		String output = "";
 		try {
